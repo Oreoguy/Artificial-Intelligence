8253378668adfcbb7a9b3070bc6e11c8a16859a4
Vansh Bhatnagar <99128036+Oreoguy@users.noreply.github.com>
2023-03-06 15:35:07 +0530

    For Constraint graph

--- CSP2.py
+++ CSP2.py
@@ -1,6 +1,72 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 
+class Node:
+    def __init__(self, domain):
+        self.domain = domain
+
+class Constraint:
+    def __init__(self, node1, node2):
+        self.node1 = node1
+        self.node2 = node2
+
+class CSP:
+    def __init__(self, root):
+        self.root = root
+        self.constraints = []
+
+    def add_constraint(self, constraint):
+        self.constraints.append(constraint)
+def pre_order(root, parent, K):
+
+    # Base condition
+    if root == None:
+        return True
+
+    # Check for the left child
+    isLeftK = root.left != None and root.left.value == K
+
+    # Check for the right child
+    isRightK = root.right != None and root.right.value == K
+
+    # Check for the parent
+    isParentK = parent != None and parent.value == K
+
+    # Updating the check variable
+    check = isLeftK or isRightK or isParentK or root.value == K
+
+    # If current node doesn't satisfy the condition, return false
+    if check == False:
+        return False
+
+    # Recur for left subtree
+    check = check and pre_order(root.left, root, K)
+
+    # Recur for right subtree
+    check = check and pre_order(root.right, root, K)
+
+    # Return the check at the end
+    return check
+def BT(csp, node, visited_nodes):
+    visited_nodes.append(node)
+    if is_leaf_node(node):
+        if is_goal_node(node):
+            return True
+        else:
+            return False
+    else:
+        for c in node.children:
+            if BT(csp, c, visited_nodes):
+                propagate_constraints(csp, c)
+                return True
+        return False
+
+
+
+visited_nodes = []
+BT(csp, root_node, visited_nodes)
+print("Visited nodes:", len(visited_nodes))
+
 # Read inputs
 #1
 N_V = int(input("Enter total number of variables: "))
@@ -72,6 +138,11 @@ for constraint in binary_constraints:
             for value2 in domains[var2]:
                 if value1 != value2:
                     graph.add_edge(var1, var2)
+
+# Example usage:
+root_node = Node([1, 2, 3])
+csp = CSP(root_node)
+# TODO: add the constraints to the CSP
 
 # Draw graph
 pos = nx.circular_layout(graph)
